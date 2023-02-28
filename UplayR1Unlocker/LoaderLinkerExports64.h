9415362861ee87bb71ad20608bbd52bdcdd045a9
Linus L <delta0lin@gmail.com>
2023-02-28 11:35:26 -0500

    update toolchain ver

--- UplayR1Unlocker/LoaderLinkerExports64.h
+++ UplayR1Unlocker/LoaderLinkerExports64.h
@@ -84,7 +84,7 @@
 #pragma comment(linker, "/export:UPLAY_SAVE_Close=uplay_r164_loader_o.UPLAY_SAVE_Close")
 #pragma comment(linker, "/export:UPLAY_SAVE_GetSavegames=uplay_r164_loader_o.UPLAY_SAVE_GetSavegames")
 #pragma comment(linker, "/export:UPLAY_SAVE_Open=uplay_r164_loader_o.UPLAY_SAVE_Open")
-#pragma comment(linker, "/export:UPLAY_SAVE_Read=uplay_r164_loader_o.UPLAY_SAVE_Read")
+// #pragma comment(linker, "/export:UPLAY_SAVE_Read=uplay_r164_loader_o.UPLAY_SAVE_Read")
 #pragma comment(linker, "/export:UPLAY_SAVE_ReleaseGameList=uplay_r164_loader_o.UPLAY_SAVE_ReleaseGameList")
 #pragma comment(linker, "/export:UPLAY_SAVE_Remove=uplay_r164_loader_o.UPLAY_SAVE_Remove")
 #pragma comment(linker, "/export:UPLAY_SAVE_SetName=uplay_r164_loader_o.UPLAY_SAVE_SetName")
